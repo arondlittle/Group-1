1a46eb188add531bdb733247c26f8ecd3c9a29fe
arondlittle <littleja@my.erau.edu>
2026-06-23 14:23:11 -0500

    Add TaskB header file with task function declaration

---
+++ include/TaskB.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    // The entry point for Task B
+    void vTaskB(void *pvParameters);
+
+#ifdef __cplusplus
+}
+#endif
