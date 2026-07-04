bc145e2267922dc8f12e53533e8ae4fdd79d01a3
arondlittle <littleja@my.erau.edu>
2026-07-04 18:08:13 -0500

    Declare semaphore and mutex in timing_shared.h
    Added semaphore and mutex declarations for shared resources.

--- src/shared/timing_shared.h
+++ src/shared/timing_shared.h
@@ -1,3 +1,12 @@
 #pragma once
 #include <cstdint>
+#include "rtos_api.h" //Semaphore file 
+
 extern volatile uint32_t g_lastHeartbeatTick;
+extern volatile int g_system_cycle_count;
+
+//Declare Mutex Handle
+extern SemaphoreHandle_t g_sharedResourceMutex;
+
+// initialization
+void InitSharedResources (); 
